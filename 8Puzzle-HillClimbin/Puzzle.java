c10e68310f02c37fb10ef2bf25b4b7e502701eeb
Fatake <pace_lozano@hotmail.com>
2021-08-25 18:34:27 -0500

    agregando tableros Iniciales y finales

--- 8Puzzle-HillClimbin/Puzzle.java
+++ 8Puzzle-HillClimbin/Puzzle.java
@@ -35,17 +35,39 @@ public class Puzzle {
 
         List<String> lineasArchivo = p.lectorArchivo(args[0]);
         int tamMatriz = Integer.parseInt(lineasArchivo.get(0));
+        int tableroInicial[][] = new int[tamMatriz][tamMatriz];
+        int tableroFinal[][] = new int[tamMatriz][tamMatriz];
 
         System.out.println("Tamaño del Tablero: "+tamMatriz);
 
         System.out.println("Tablero Inicial:");
         for (int i = 1; i < tamMatriz+1; i++) {
-            System.out.println(lineasArchivo.get(i));
+            // de la fila se sacan los elementos de la columna
+            String[] aux = lineasArchivo.get(i).split(",");
+            int j = 0;
+            // se rellena cada columna
+            for (String value : aux) {
+                tableroInicial[i-1][j] = Integer.parseInt(value);
+                System.out.print(tableroInicial[i-1][j]+ " ");
+                j++;
+            }
+            System.out.println();
         }
 
         System.out.println("Tablero Final:");
+        int k = 0;
         for (int i = lineasArchivo.size()-1; i > lineasArchivo.size()-tamMatriz-1; i--) {
-            System.out.println(lineasArchivo.get(i));
+            String[] aux = lineasArchivo.get(i).split(",");
+
+            int j = tamMatriz-1;
+            // se rellena cada columna
+            for (String value : aux) {
+                tableroFinal[k][j] = Integer.parseInt(value);
+                System.out.print(tableroFinal[][j]+ " ");
+                j--;
+            }
+            System.out.println();
+            k++;
         }
 
     }

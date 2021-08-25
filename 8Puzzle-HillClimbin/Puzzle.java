d57f0ad13bec789752eefc719320461ac3f15038
Fatake <pace_lozano@hotmail.com>
2021-08-25 18:40:07 -0500

    impresion de tablero concluida

--- 8Puzzle-HillClimbin/Puzzle.java
+++ 8Puzzle-HillClimbin/Puzzle.java
@@ -7,6 +7,26 @@ import java.util.Scanner;
 
 public class Puzzle {
 
+    /**
+     * Funcion printTablero
+     * Dado Un Int[][] Imprime como se ve
+     * @param tablero
+     */
+    private void printTablero(int tablero[][]){
+        for (int i = 0; i < tablero.length; i++) {
+            for (int j = 0; j < tablero.length; j++) {
+                System.out.print(tablero[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    /**
+     * Funcion lectorArchivo
+     * Lee un archivo y regresa una lista las lINEAS LEIDAS
+     * @param nombreArchivo
+     * @return
+     */
     private List<String > lectorArchivo(String nombreArchivo) {
         File archivo = new File(nombreArchivo);
         List<String> lineas = new ArrayList<String>();
@@ -28,6 +48,8 @@ public class Puzzle {
     public static void main(String[] args) {
         Puzzle p = new Puzzle();
         System.out.println("Programa que explora el 8puzzle");
+
+        // Checa los Argumentos
         if (args.length < 0) {
             System.out.println("[!] No paso un archivo como argumento");
             System.exit(1);
@@ -38,9 +60,6 @@ public class Puzzle {
         int tableroInicial[][] = new int[tamMatriz][tamMatriz];
         int tableroFinal[][] = new int[tamMatriz][tamMatriz];
 
-        System.out.println("Tamaño del Tablero: "+tamMatriz);
-
-        System.out.println("Tablero Inicial:");
         for (int i = 1; i < tamMatriz+1; i++) {
             // de la fila se sacan los elementos de la columna
             String[] aux = lineasArchivo.get(i).split(",");
@@ -48,27 +67,27 @@ public class Puzzle {
             // se rellena cada columna
             for (String value : aux) {
                 tableroInicial[i-1][j] = Integer.parseInt(value);
-                System.out.print(tableroInicial[i-1][j]+ " ");
                 j++;
             }
-            System.out.println();
         }
 
-        System.out.println("Tablero Final:");
-        int k = 0;
+        int k = tamMatriz-1;
         for (int i = lineasArchivo.size()-1; i > lineasArchivo.size()-tamMatriz-1; i--) {
             String[] aux = lineasArchivo.get(i).split(",");
 
-            int j = tamMatriz-1;
+            int j = 0;
             // se rellena cada columna
             for (String value : aux) {
                 tableroFinal[k][j] = Integer.parseInt(value);
-                System.out.print(tableroFinal[][j]+ " ");
-                j--;
+                j++;
             }
-            System.out.println();
-            k++;
+            k--;
         }
 
+        System.out.println("Tamaño del Tablero: " + tamMatriz);
+        System.out.println("Tablero Inicial:");
+        p.printTablero(tableroInicial);
+        System.out.println("Tablero Final:");
+        p.printTablero(tableroFinal);
     }
 }
